f90179405030fe0a4f4fc9bf209897ec0e4877ae
manojna655 <mudunuri.manojna@gmail.com>
2026-06-24 19:22:56 +0530

    Update q6.py

---
+++ q6.py
@@ -0,0 +1,6 @@
+#Join two strings "Hello" and "World" with a space in between. Find the length of the new string. 
+
+str1 = "Hello"
+str2 = "World"
+new_str = str1 + " " + str2
+print(len(new_str))
