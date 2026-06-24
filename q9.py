47a5b25205e0b8cc56cab78bc5823fb43e8105c9
manojna655 <mudunuri.manojna@gmail.com>
2026-06-24 19:23:49 +0530

    Update q9.py

---
+++ q9.py
@@ -0,0 +1,7 @@
+#Given 2 variables: language = “Python” version = 3.13 
+language = "Python"
+version = 3.13
+#Print the following output using the above variables:
+print(f"{language} version {version}")
+
+    
