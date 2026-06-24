8502b5128417ec1591b3bf8e09e0118face54958
manojna655 <mudunuri.manojna@gmail.com>
2026-06-24 19:22:07 +0530

    Update q3.py

---
+++ q3.py
@@ -0,0 +1,4 @@
+# Create a tuple with 4 numbers and print its last element and second element. 
+b = (1, 2, 3, 4)
+print(b[-1])  # prints the last element 
+print(b[1])   # prints the second element
