07a73d7bb8504bac156a70d8c22fdf922ca746d8
manojna655 <mudunuri.manojna@gmail.com>
2026-06-24 19:21:11 +0530

    Update 12.py

---
+++ 12.py
@@ -0,0 +1,16 @@
+# Create the following variables: attendance = True assignment_submitted = False a. Which operator should be applied between these variables to get a True? b. Which operator should be applied between these variables to get a False? c. Which operator should be applied to ‘attendance’ to get a False? 
+attendance = True
+assignment_submitted = False
+
+# a. Which operator should be applied between these variables to get a True?
+# Answer: or operator
+result1 = attendance or assignment_submitted
+
+# b. Which operator should be applied between these variables to get a False?
+# Answer: and operator
+result2 = attendance and assignment_submitted
+
+# c. Which operator should be applied to 'attendance' to get a False?
+# Answer: not operator
+result3 = not attendance
+
